cc14d8e2cf0e7e66e7c6c50fcc5b104007e3f746
Ryan Fuller <rfuller250@yahoo.com>
2016-10-17 12:08:12 -0500

    Updated data/ Directory

--- data/getArticles.py
+++ data/getArticles.py
@@ -1,30 +1,61 @@
 import json
 import requests
 
+# Pull articles from News API & Newslookup API
+def getAllArticles():
+   getNewsAPIArticles()
+   getNewslookupArticles()
+
+
 # News API
 # Pulls all articles for all sources
 # Format: {source_id:[articles_list]}
-def getAllArticles():
-   with open('api_data/news_api_sources_id_list', 'r') as f:
-      source_list = json.load(f)
+def getNewsAPIArticles():
+   with open('api_data/news_api_sources_id_map.json', 'r') as f:
+      source_map = json.load(f)
       
    apiKey = '836d24a3cd7c43bd9450a4496c2dbf41'
    url_part1 = 'https://newsapi.org/v1/articles?source='
    url_part2 = '&apiKey=' + apiKey
 
    allArticles = { }
-   for source in source_list:
+   for source in source_map.keys():
       response = requests.get(url_part1 + source + url_part2)
       response = response.json()
       articles = response['articles']
       allArticles[source] = articles
       
-   with open('api_data/news_api_all_articles', 'w') as f:
+   with open('api_data/news_api_all_articles.json', 'w') as f:
       json.dump(allArticles, f)
 
+# Newslookup API
+# Pulls all articles for all regions
+# Format: {region:[articles_list]}
+def getNewslookupArticles():
+	with open('api_data/newslookup_regions_id_map.json', 'r') as f:
+		region_map = json.load(f)
 
+	client_id = '17122'
+	secret = '32c77c1b83556dbf3c621247302af153'
+	url_part1 = 'https://api.newslookup.com/feed/live?fmt=&region='
+	url_part2 = '&output=json&client_id='
+	url_part3 = '&secret='
+
+	allArticles = { }
+	for region in region_map.keys():
+		response = requests.get(url_part1 + region + url_part2 + client_id + url_part3 + secret)
+		response = response.json()
+		articles = response['items']
+		allArticles[region_map[region]] = articles
+
+	with open('api_data/newslookup_api_all_articles.json', 'w') as f:
+		json.dump(allArticles, f)
+
+
+# Temporary
+# Pulls 3 specific articles, sources, and locations
 def getSampleData():
-	with open('api_data/news_api_all_articles', 'r') as f:
+	with open('api_data/news_api_all_articles.json', 'r') as f:
 		articles = json.load(f)
 
 	with open('api_data/news_api_sources.json', 'r') as f:
@@ -63,15 +94,15 @@ def getSampleData():
 	sample_sources = [source1, source2, source3]
 	sample_countries = [country1, country2, country3]
 
-	with open('api_sample_data/sample_articles.json', 'w') as f:
+	with open('api_data/sample_articles.json', 'w') as f:
 		json.dump(sample_articles, f)
 
-	with open('api_sample_data/sample_sources.json', 'w') as f:
+	with open('api_data/sample_sources.json', 'w') as f:
 		json.dump(sample_sources, f)
 
-	with open('api_sample_data/sample_countries.json', 'w') as f:
+	with open('api_data/sample_countries.json', 'w') as f:
 		json.dump(sample_countries, f)
 
 if __name__ == '__main__':
-   #getAllArticles()
-   getSampleData()
+   getAllArticles()
+   #getSampleData()
