99fa2245dbdebfc3d76f19189390cddbaeb147c8
Ryan Fuller <rfuller250@yahoo.com>
2016-10-19 12:56:25 -0500

    Working on Links and Routing

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', articles=articles)
 
 @app.route('/about')
 def about():
