0d246c2bdf94dde4558882d07fba251a48d02e21
Ryan Fuller <rfuller250@yahoo.com>
2016-11-09 10:40:53 -0600

    Finished auto-update

--- data/getData.py
+++ data/getData.py
@@ -217,7 +217,7 @@ def printEmailSummary(numArticles1, numArticles2, elapsedTime):
    print('Runtime = ' + elapsedTime + ' sec')
    print(str(numArticles1) + ' articles pulled from News API')
    print(str(numArticles2) + ' articles pulled from Newslookup API')
-   print(str(numArticles1 + numArticles2) + ' total aricles pulled')
+   print(str(numArticles1 + numArticles2) + ' total articles pulled')
    print('getData Execution Success')
 
 ##########################
